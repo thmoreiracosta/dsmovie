fd5897fcd9b7695b494735e1eb7782bd43a55ea6
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-11 08:56:17 -0300

    SecurityConfig

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -32,8 +32,7 @@ function Listing() {
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
                         <MovieCard />
-                    </div>
-                    
+                    </div>                
                     
                 </div>
             </div>
