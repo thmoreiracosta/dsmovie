c3ba205a7af621cc4396b9d8f22fc56af58a9c5f
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-12 16:21:01 -0300

    atualizações

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -11,6 +11,7 @@ function Form() {
         score: 4.5
     };
 
+
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
