1d1c856905218b38c41b21f8d2f9176b52bde3d6
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-13 15:39:54 -0300

    Save, Navigate

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,24 @@
-import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
-import './styles.css';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import { Link } from 'react-router-dom'
+import './styles.css'
 function Navbar() {
-
-    return (
-        <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <a href="https://thmoreiracosta.netlify.app/"><h1>ThMovies</h1></a>
-                        <a href="https://github.com/thmoreiracosta">
-                            <div className="dsmovie-contac-container">
-                                <GithubIcon />
-                            <p className="dsmovie-contact-link"> /thmoreiracosta</p>
-                        </div>
-                    </a>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <Link to="/">
+            <h1>ThMovies</h1>
+          </Link>
+          <a href="https://github.com/thmoreiracosta">
+            <div className="dsmovie-contac-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link"> /thmoreiracosta</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
-export default Navbar;
+export default Navbar
