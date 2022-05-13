3457d48a798531ccbb01cf27119f6291ffa40e59
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-13 11:19:19 -0300

    início Props_cor

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://thmoreiracosta.netlify.app/";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://thmoreiracosta.netlify.app";
 
