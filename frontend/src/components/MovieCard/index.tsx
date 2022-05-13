7b6d3b7c96e42058b94dbea883bcbf0801e77726
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-13 14:18:45 -0300

    showScore

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,27 +1,29 @@
-import { Link } from "react-router-dom";
-import MovieScore from "../MovieScore";
-import { Movie } from "types/movie";
+import { Link } from 'react-router-dom'
+import MovieScore from '../MovieScore'
+import { Movie } from 'types/movie'
 
 type Props = {
-    movie: Movie;
+  movie: Movie
 }
 
 function MovieCard({ movie }: Props) {
+  return (
+    <div>
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <MovieScore count={movie.count} score={movie.score} />
 
-    return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore />
-
-                <Link to={`/form/${movie.id}`}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </Link>
-
-            </div>
-        </div>
-    );
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default MovieCard;
+export default MovieCard
--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,19 +1,21 @@
-import MovieStars from "../MovieStars";
-import './styles.css';
+import MovieStars from '../MovieStars'
+import './styles.css'
 
-function MovieScore() {
-
-    const score = 3.5;
-    const count = 13;
-
-    return (
-        <div className="dsmovie-score-container">
-            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
-            <p className="dsmovie-score-count">{count} avaliações</p>
-        </div>
-
-    );
+type Props = {
+  score: number
+  count: number
 }
 
-export default MovieScore;
+function MovieScore({ score, count }: Props) {
+  return (
+    <div className="dsmovie-score-container">
+      <p className="dsmovie-score-value">
+        {score > 0 ? score.toFixed(1) : '-'}
+      </p>
+      <MovieStars score={score} />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+  )
+}
+
+export default MovieScore
