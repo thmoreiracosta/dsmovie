3fa62a17368165dc6c2ba63c6d3d524624a0eace
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-13 09:55:59 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,17 +2,25 @@ import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    // FORMA ERRADA DE FAZER REQUISIÇÃO (TESTE)
-    axios.get(`${BASE_URL}/movies`)
-        .then(response => {
-            console.log(response.data);
-        })
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
