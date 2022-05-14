eccc9489fa889511455f23ad379c1ef2daa742fb
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-13 23:21:35 -0300

    Atualizaçoes - melhoriaOne

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import { ReactComponent as LinkedIn } from 'assets/img/linkedin-logo.svg'
 import { Link } from 'react-router-dom'
 import './styles.css'
 function Navbar() {
@@ -9,16 +10,24 @@ function Navbar() {
           <Link to="/">
             <h1>ThMovies</h1>
           </Link>
+          <div className="dsmovie-social-container">
+                        <a href="https://www.linkedin.com/in/thmoreiracosta/" target='_blank' rel='nooppener noreferrer'  >
+                            <div className="dsmovie-contact-container">
+                                <LinkedIn />
+                                <p className="dsmovie-contact-link1">/thmoreiracosta</p>
+                            </div>
+                        </a>
           <a href="https://github.com/thmoreiracosta">
             <div className="dsmovie-contac-container">
               <GithubIcon />
               <p className="dsmovie-contact-link"> /thmoreiracosta</p>
             </div>
           </a>
+          </div>
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
